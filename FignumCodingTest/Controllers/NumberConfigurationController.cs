d33fe417cd79c56f6c7ab2905e5e59a003158153
Alessandro Leo <alessandro.leo@iar.com>
2023-08-23 13:16:04 +0100

    FignumCodingTest: log and readme

--- FignumCodingTest/Controllers/NumberConfigurationController.cs
+++ FignumCodingTest/Controllers/NumberConfigurationController.cs
@@ -19,16 +19,26 @@ namespace FignumCodingTest.Controllers
         [HttpPost]
         public async Task<ActionResult<Request>> Execute(Request request)
         {
-            var result = await NumberConfigurationHelper.RemovePrimeNumbersFilter(request.InputStringNumber);
-
-            result = await NumberConfigurationHelper.SortFilter(result);
-
-            if (result.CheckAndSortedList != null)
+            try
             {
-                return Ok(result);
-            }
+                var result = await NumberConfigurationHelper.RemovePrimeNumbersFilter(request.InputStringNumber);
 
-            return BadRequest(result);
+                result = await NumberConfigurationHelper.SortFilter(result);
+
+                if (result.CheckAndSortedList != null)
+                {
+                    _logger.LogInformation($"OK {result}");
+                    return Ok(result);
+                }
+
+                _logger.LogWarning($"Bad request {result}");
+                return BadRequest(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(500, $"Something went wrong - {ex.Message}");
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# FignumCodingTest
+## FignumCodingTest
+
+## Technical specification
+This project requires .NET 7 SDK. The solution `FignumCodingTest.sln` contains two projects `FignumCodingTest` and `TestFignumCodingTest.Test`.
+The frontend we will use to test is Swagger UI.
+
+
+## Alex Leo Notes
+No authentication or authorization has been implemented - potentially we could use Azure API Management AAD.
+We might need to implement some sort of caching in order to improve performance  - store request into database.
+As a proof of concept, I have added some logging - output to console. Moving forward a persistent logging must be implemented - perhaps using Serilog, Log4Net or any other alternatives.
